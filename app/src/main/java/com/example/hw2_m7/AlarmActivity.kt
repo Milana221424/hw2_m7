6530ca58ad8b7845d4efd3c9ad2431efac602fa7
ZhannaSap <nekotyan474@gmail.com>
2024-02-19 12:45:54 +0600

    changed alarm

--- app/src/main/java/com/example/hw2_m7/AlarmActivity.kt
+++ app/src/main/java/com/example/hw2_m7/AlarmActivity.kt
@@ -28,108 +28,14 @@ class AlarmActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAlarmBinding
     private var job: Job? = null
-    private lateinit var picker: MaterialTimePicker
-    private lateinit var calendar: Calendar
-    private lateinit var alarmManager: AlarmManager
-    private lateinit var pendingIntent: PendingIntent
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        job = CoroutineScope(Dispatchers.Main).launch{
-
-            createNotificationChannel()
-
-            binding.btnSelectTime.setOnClickListener { showTimePicker() }
-            binding.btnSetAlarm.setOnClickListener { setAlarm() }
-            binding.btnCancelAlarm.setOnClickListener { cancelAlarm() }
-        }
-
-
-    }
-
-    @SuppressLint("UnspecifiedImmutableFlag")
-    private fun cancelAlarm() {
-        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
-        val  intent = Intent(this,AlarmReceiver::class.java)
-        pendingIntent = PendingIntent.getBroadcast(this, 0, intent,0)
-        alarmManager.cancel(pendingIntent)
-        Toast.makeText(this,"Alarm has canceled", Toast.LENGTH_SHORT).show()
-    }
-
-    @SuppressLint("UnspecifiedImmutableFlag")
-    private fun setAlarm() {
-        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
-        val  intent = Intent(this,AlarmReceiver::class.java)
-        pendingIntent = PendingIntent.getBroadcast(this, 0, intent,0)
-
-        alarmManager.setRepeating(
-            AlarmManager.RTC_WAKEUP,calendar.timeInMillis,
-            AlarmManager.INTERVAL_DAY, pendingIntent
-        )
-
-        Toast.makeText(this,"Alarm set successfully", Toast.LENGTH_SHORT).show()
-
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun showTimePicker() {
-        picker = MaterialTimePicker.Builder()
-            .setTimeFormat(TimeFormat.CLOCK_12H)
-            .setHour(12)
-            .setMinute(0)
-            .setTitleText("Select alarm time")
-            .build()
-
-        picker.show(supportFragmentManager, "foxandroid")
-
-        picker.addOnPositiveButtonClickListener {
-            if (picker.hour > 12) {
-                binding.tvAlarmTime.setText( String.format("%02d", picker.hour - 12) + " : " + String.format(
-                    "%02d",
-                    picker.minute
-                ) + " PM")
-
-            } else {
-                binding.tvAlarmTime.setText( String.format("%02d", picker.hour) + " : " + String.format(
-                    "%02d",
-                    picker.minute
-                ) + " AM")
-
-            }
-
-            calendar = Calendar.getInstance()
-            calendar[Calendar.HOUR_OF_DAY] = picker.hour
-            calendar[Calendar.MINUTE] = picker.minute
-            calendar[Calendar.SECOND] = 0
-            calendar[Calendar.MILLISECOND] = 0
-        }
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name: CharSequence = "foxandroidReminderChannel"
-            val description = "Channel for Alarm"
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel("foxandroid", name, importance)
-            channel.description = description
-            val notificationManager = getSystemService(
-                NotificationManager::class.java
-            )
-
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        job?.cancel()
-    }
-}
-
-/* binding.btnSelectTime.setOnClickListener {
+        binding.btnSelectTime.setOnClickListener {
             val materialTimerPicker =
                 MaterialTimePicker.Builder().setInputMode(MaterialTimePicker.INPUT_MODE_KEYBOARD)
                     .setTimeFormat(TimeFormat.CLOCK_12H)
@@ -149,21 +55,34 @@ class AlarmActivity : AppCompatActivity() {
 
                 val currentTime = Calendar.getInstance().timeInMillis
                 val alarmTime = calendar.timeInMillis
-                binding.tvAlarmTime.text = "${materialTimerPicker.hour} : ${materialTimerPicker.minute}"
+                binding.tvAlarmTime.text =
+                    "${materialTimerPicker.hour} : ${materialTimerPicker.minute}"
                 val delayMills = alarmTime - currentTime
 
                 if (delayMills > 0) {
-                    Log.e("ololo", "delayMills: $delayMills", )
+                    Log.e("ololo", "delayMills: $delayMills")
                     job = CoroutineScope(Dispatchers.Main).launch {
                         Log.e("ololo", "Delay in proccess, $delayMills")
-                        showToast()
                         delay(delayMills)
                         Log.e("ololo", "Delay completed, calling showToast()")
                         showToast()
                     }
                 } else {
-                    Log.e("ololo", "onCreate: else is working", )
+                    Log.e("ololo", "onCreate: else is working")
                     Toast.makeText(this, "Выберите время", Toast.LENGTH_SHORT).show()
                 }
             }
-        }*/
+        }
+    }
+
+    private fun showToast() {
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(this@AlarmActivity, "Будильник сработал", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job?.cancel()
+    }
+}
